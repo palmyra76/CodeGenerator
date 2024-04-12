d48260576395c7b5c594390c43882464170835f7
Raja K <ksvraja@gmail.com>
2024-04-12 09:54:05 +0530

    Command line runner added

---
+++ API/projectgenerator/src/main/java/com/example/palmyralabs/projectgenerator/CodeGen.java
@@ -0,0 +1,12 @@
+package com.example.palmyralabs.projectgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGen {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGeneratorApplication.class, args);	
+	}
+}
